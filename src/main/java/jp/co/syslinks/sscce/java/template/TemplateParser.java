3d5cf6f3416897079184a203ef3dfb0861c6e498
koryo <koryovip@yahoo.co.jp>
2021-04-17 21:08:56 +0900

    fix

--- src/main/java/jp/co/syslinks/sscce/java/template/TemplateParser.java
+++ src/main/java/jp/co/syslinks/sscce/java/template/TemplateParser.java
@@ -25,18 +25,7 @@ public class TemplateParser {
 
     private Status stauts = Status.contents;
 
-    public void parse(Reader reader) throws Exception {
-        Map<String, Object> map = new HashMap<>();
-        map.put("user", "suzuki");
-        map.put("items", Arrays.asList(new String[] { "1", "2", "3" }));
-        map.put("userList",
-                Arrays.asList(new USER[] { //
-                        new USER("wang", 20, 1) //
-                        , new USER("zhang", 22, 0) //
-                        , new USER("li", 30, 0) //
-                        , new USER("sun", 42, 1) //
-                }));
-
+    public void parse(Reader reader, Map<String, Object> map) throws Exception {
         List<TokenBase> tokenList = new ArrayList<>();
         StringBuilder buff = new StringBuilder();
         int read = -1;
@@ -109,9 +98,9 @@ public class TemplateParser {
         for (int ii = 0, len = tokenList.size(); ii < len; ii++) {
             TokenBase token = tokenList.get(ii);
             if (token instanceof TokenContents) {
-                System.out.print(((TokenContents) token).contents);
+                append(((TokenContents) token).contents);
             } else if (token instanceof TokenValue) {
-                System.out.print(getValue(((TokenValue) token), map, null));
+                append(getValue(((TokenValue) token), map, null));
             } else if (token instanceof TokenFor) {
                 TokenFor tokenFor = (TokenFor) token;
                 Object obj = map.get(tokenFor.items);
@@ -121,15 +110,19 @@ public class TemplateParser {
                     for (int jj = ii + 1; jj < tokenFor.endIndex; jj++) {
                         TokenBase token2 = tokenList.get(jj);
                         if (token2 instanceof TokenContents) {
-                            System.out.print(((TokenContents) token2).contents);
+                            append(((TokenContents) token2).contents);
                         } else if (token2 instanceof TokenValue) {
-                            System.out.print(getValue(((TokenValue) token2), map, item));
+                            append(getValue(((TokenValue) token2), map, item));
                         }
                     }
                 }
                 ii = tokenFor.endIndex;
             }
         }
+    }
+
+    private void append(Object str) {
+        System.out.print(str);
     }
 
     private Object getValue(TokenValue tokenValue, Map<String, Object> map, Object item) throws Exception {
@@ -142,15 +135,26 @@ public class TemplateParser {
         return item.getClass().getField(tokenValue.key2.substring(1)).get(item);
     }
 
-    private static final Pattern VALUE = Pattern.compile("^([a-zA-Z0-9]+)(\\.[a-zA-Z0-9]+)?$", Pattern.MULTILINE);
+    private static final Pattern VALUE = Pattern.compile("^([a-zA-Z0-9]+)(\\.[a-zA-Z0-9]+)?$"/*, Pattern.MULTILINE*/);
     private static final Pattern FOR1 = Pattern.compile("for[ ]+([a-zA-Z0-9]+)[ ]+:[ ]+([a-zA-Z0-9]+)");
 
     public static void main(String[] args) throws Exception {
         try (Reader reader = new InputStreamReader(TemplateParser.class.getResourceAsStream("template.html"))) {
-            new TemplateParser().parse(reader);
+            Map<String, Object> map = new HashMap<>();
+            map.put("user", "suzuki");
+            map.put("items", Arrays.asList(new String[] { "1", "2", "3" }));
+            map.put("userList",
+                    Arrays.asList(new USER[] { //
+                            new USER("wang", 20, 1) //
+                            , new USER("zhang", 22, 0) //
+                            , new USER("li", 30, 0) //
+                            , new USER("sun", 42, 1) //
+                    }));
+            new TemplateParser().parse(reader, map);
         }
     }
 
+    @SuppressWarnings("unused")
     private static final class USER {
         public final String name;
         public final int age;
